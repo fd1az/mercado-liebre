1906109b1b0a96458bbcb4e2f02e739239a2302c
fd1az <fdiaz.dev@gmail.com>
2022-03-24 19:34:39 -0300

    PORT

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const PORT = process.env.PORT || 4001;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -15,4 +15,4 @@ app.get("/register", (req, res) => {
   res.sendFile(__dirname + "/views/register.html");
 });
 
-app.listen(4001, () => console.log("running on port 4001"));
+app.listen(PORT, () => console.log("running on port 4001"));
