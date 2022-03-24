3daf3a64c9e13995edaa578a9e8a76c80a9759f8
fd1az <fdiaz.dev@gmail.com>
2022-03-24 19:31:15 -0300

    chelo

--- app.js
+++ app.js
@@ -7,4 +7,12 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/home.html");
 });
 
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
+});
+
 app.listen(4001, () => console.log("running on port 4001"));
